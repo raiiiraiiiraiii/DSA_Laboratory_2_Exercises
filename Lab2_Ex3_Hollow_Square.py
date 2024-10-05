0b142bb3fe41c659f9ab81c79b49bd135706c983
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-06 00:07:02 +0800

    Create the main program

--- Lab2_Ex3_Hollow_Square.py
+++ Lab2_Ex3_Hollow_Square.py
@@ -4,3 +4,8 @@
 length = int(input("Enter the side length of the square: "))
 
 # Main function
+for i in range (length):
+    if i == 0 or i == (length-1):
+        print('x' * length)
+    else:
+        print('x' + ' ' * (length-2) + 'x')
