a2c90c7309549143eb414db8c5271c30711f8aa0
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-05 20:54:42 +0800

    Calculate the powers

--- Lab2_Ex1_Powers.py
+++ Lab2_Ex1_Powers.py
@@ -1,6 +1,12 @@
 # Calculation of Powers
 
 # Main: Calculating the powers
+def calculate_powers(base, exponent): 
+    if exponent == 0:
+        return 1
+    else:
+        return base * exponent(base, exponent - 1)
+
 # Input the base
-# Input the power
+# Input the exponent
 # Display the result
