7a7d9a6b540085ebc4ac173d45006c1ac71d8730
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-05 23:13:13 +0800

    Ask user array size

--- Lab2_Ex2_Cube_Int.py
+++ Lab2_Ex2_Cube_Int.py
@@ -1,5 +1,7 @@
 # Cube of Integers
 
 # Ask user how many elements in the array
+size_array = int(input("Enter the size of the array: "))
+
 # Ask user to input the elements (separated by space)
 # Display the results
