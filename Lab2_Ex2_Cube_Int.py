3866b94e65d51d6f38cf1ecf5245f36bdaf49815
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-05 23:52:06 +0800

    Main

--- Lab2_Ex2_Cube_Int.py
+++ Lab2_Ex2_Cube_Int.py
@@ -1,7 +1,26 @@
 # Cube of Integers
 
 # Ask user how many elements in the array
-size_array = int(input("Enter the size of the array: "))
+while True:
+    try:
+        size_array = int(input("Enter the size of the array: "))
+        print
+        break
+    except ValueError:
+        print("Invalid input. Enter an integer.")
 
 # Ask user to input the elements (separated by space)
-# Display the results
+while True:
+
+    input_elements_string = str(input(f"Enter {size_array} elements separated by space: "))
+
+    input_elements_int = input_elements_string.split()
+
+    if len(input_elements_int) == size_array:
+        integer_elements = [int(elements) for elements in input_elements_int]
+    
+        for number in integer_elements:
+            print(number ** 3)
+        break
+    else:
+        print(f"\nERROR: Enter exactly {size_array} elements.")
