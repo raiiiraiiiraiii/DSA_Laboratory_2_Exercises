5eb6a1a585d9ee327f2668725a55babb44b43cb7
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-05 21:38:39 +0800

    Display result and fix errors

--- Lab2_Ex1_Powers.py
+++ Lab2_Ex1_Powers.py
@@ -5,12 +5,12 @@ def calculate_powers(base, exponent):
     if exponent == 0:
         return 1
     else:
-        return base * exponent(base, exponent - 1)
+        return base * calculate_powers(base, exponent - 1)
 
 # Input the base
 while True:
     try:
-        base = int(input("Enter base number: "))
+        input_base = int(input("Enter base number: "))
         break
     except ValueError:
         print("Invalid input. Enter an integer.")
@@ -18,10 +18,12 @@ while True:
 # Input the exponent
 while True:
     try:
-        exponent = int(input("Enter exponent: "))
+        input_exponent = int(input("Enter exponent: "))
         break
     except ValueError:
         print("Invalid input. Enter an integer.")
 
-powers_result = calculate_powers(base, exponent)
+powers_result = calculate_powers(input_base, input_exponent)
+
 # Display the result
+print(f"{input_base}^{input_exponent} = {powers_result}")
